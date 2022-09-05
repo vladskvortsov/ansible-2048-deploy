e11c8800a61f6dc9e6a358cbd67cbac76f3be352
vladskvortsov <vladskvortsov96@gmail.com>
2022-09-06 00:44:20 +0300

    update

--- ansible-2048-deploy.sh
+++ ansible-2048-deploy.sh
@@ -10,3 +10,6 @@ cd ansible-2048-deploy
 sudo ansible-playbook playbook.yml -i /home/ubuntu/ansible-2048-deploy/hosts.ini -l controller -u root
 
 #sudo scp -r -i /home/vlad/Завантаження/LightsailDefaultKey-eu-west-2.pem ubuntu@3.9.90.3:* /home/vlad/ansible-2048-deploy/
+
+#curl https://ipinfo.io/ip
+
