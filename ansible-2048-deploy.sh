6ae3ec77dc064944818977c7af5802303fa26dbc
vladskvortsov <vladskvortsov96@gmail.com>
2022-09-08 05:36:35 +0300

    domain name fix

--- ansible-2048-deploy.sh
+++ ansible-2048-deploy.sh
@@ -7,11 +7,16 @@ git clone https://github.com/vladskvortsov/ansible-2048-deploy.git
 
 cd ansible-2048-deploy
 
-sudo ansible-playbook playbook.yml -i /home/ubuntu/ansible-2048-deploy/hosts.ini -l controller -u root
+sudo ansible-playbook playbook.yml -i /home/ubuntu/ansible-2048-deploy/hosts.ini -l controller -u ubuntu
 
 #sudo scp -r -i /home/vlad/Завантаження/LightsailDefaultKey-eu-west-2.pem ubuntu@3.9.90.3:* /home/vlad/ansible-2048-deploy/
 
 #curl https://ipinfo.io/ip
 
-#sudo sed -i 's/::1 ip6-localhost ip6-loopback/::1 ip6-localhost ip6-loopback ip6-oldgood2048.ga/' /etc/hosts
-#sudo sed -i 's/127.0.0.1 localhost/127.0.0.1 localhost oldgood2048.ga/' /etc/hosts
+
+
+#apk --update add certbot certbot-nginx nano curl
+#certbot --nginx -d oldgood2048.ga -d www.oldgood2048.ga
+#certbot renew --dry-run
+#nginx -s reload
+
